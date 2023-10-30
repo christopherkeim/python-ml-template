a5674d5914d984809b33dfb5980a888fce26d83f
Christopher Keim <christopher.jl.keim@gmail.com>
2023-10-30 13:03:05 -0400

    Docker and data science CLI tools.

--- setup.sh
+++ setup.sh
@@ -1,18 +1,38 @@
 #! /bin/bash
+# 
 # This script sets up a local development environment on an Ubuntu 20.04/22.04 machine
 # to work with Poetry managed Python3.10 projects. 
 # 
 # Targets:
 #   - Poetry 1.5.1
 #   - Python3.10
+#   - Docker 24.0.6
 #
 # Requirements:
 #   - Ubuntu 20.04/22.04
 #   - Python3.7+ 
 #
 # -----------------------------------------------------------------------------------------------------------
-# 1) Base Requirements: this will ensure that you have curl and make installed.
+# 1) Base Requirements: this will ensure that you base requirements along with some data science
+#    CLI tools installed.
 # -----------------------------------------------------------------------------------------------------------
+
+# Check if ca-certificates is in the apt-cache
+if ( apt-cache show ca-certificates > /dev/null )
+then
+  echo "ca-certificates is already cached 🟢"
+else
+  sudo apt update
+fi
+
+# Ensure ca-certificates package is installed on the machine
+if ( which update-ca-certificates > /dev/null )
+then
+  echo "ca-certificates is already installed 🟢"
+else
+  echo "Installing ca-certificates 📜"
+  sudo apt-get install -y ca-certificates
+fi
 
 # Check if curl is in the apt-cache
 if ( apt-cache show curl > /dev/null )
@@ -23,7 +43,7 @@ else
 fi
 
 # Ensure curl is installed on the machine
-if [ -n "$(which curl)" ]
+if ( which curl > /dev/null )
 then
   echo "curl is already installed 🟢"
 else
@@ -40,7 +60,7 @@ else
 fi
 
 # Ensure make is installed on the machine
-if [ -n "$(which make)" ]
+if ( which make > /dev/null )
 then
   echo "make is already installed 🟢"
 else
@@ -48,38 +68,78 @@ else
   sudo apt install -y make
 fi
 
-# -----------------------------------------------------------------------------------------------------------
-# 2) Poetry Install: here we'll install and configure Poetry, as well as add Poetry to the PATH.
-# -----------------------------------------------------------------------------------------------------------
-
-# Install Poetry using the official installer
-if [ -n "$(which poetry)" ]
+# Check if gnupg is in the apt-cache
+if ( apt-cache show gpg > /dev/null )
 then
-  echo "Poetry is already installed 🟢"
+  echo "gnupg is already cached 🟢"
 else
-  echo "Installing Poetry 🧙‍♂️"
-  curl -sSL https://install.python-poetry.org | python3 -
+  sudo apt update
 fi
 
-# Add Poetry to the path in the current user's .bashrc
-if ( poetry --version > /dev/null )
+# Ensure gnupg is installed on the machine
+if ( which gpg > /dev/null )
 then
-  echo "Poetry is already in PATH 🟢"
+  echo "make is already installed 🟢"
 else
-  echo -e "# Add Poetry (Python Package Manager) to PATH\nexport PATH="/home/$USER/.local/bin:$PATH"" >> ~/.bashrc
+  echo "Installing gnugp 🔧"
+  sudo apt install -y gnupg
 fi
 
-# Configure Poetry to put build all virtual environments in the project's directory
-if [ "$(poetry config virtualenvs.in-project)" == "true" ]
+# Check if bat is in the apt-cache
+if ( apt-cache show bat > /dev/null )
 then
-  echo "Poetry already configured to create virtual envs within projects 🟢"
+  echo "bat is already cached 🟢"
 else
-  echo "Configuring Poetry to create virtual envs in projects 🪐"
-  poetry config virtualenvs.in-project true
+  sudo apt update
 fi
+
+# Ensure bat is installed on the machine
+if ( which bat > /dev/null )
+then
+  echo "bat is already installed 🟢"
+else
+  echo "Installing bat 🔧"
+  sudo apt install -y bat
+fi
+
+
+# Check if bat is in the apt-cache
+if ( apt-cache show jq > /dev/null )
+then
+  echo "jq is already cached 🟢"
+else
+  sudo apt update
+fi
+
+# Ensure bat is installed on the machine
+if ( which jq > /dev/null )
+then
+  echo "jq is already installed 🟢"
+else
+  echo "Installing jq 🔧"
+  sudo apt install -y jq
+fi
+
+# Check if csvkit is in the apt-cache
+if ( apt-cache show csvkit > /dev/null )
+then
+  echo "csvkit is already cached 🟢"
+else
+  sudo apt update
+fi
+
+# Ensure csvkit is installed on the machine (commands csvlook, csvcut, in2csv, sql2csv)
+if ( which csvlook > /dev/null )
+then
+  echo "csvkit is already installed 🟢"
+else
+  echo "Installing csvkit 🔧"
+  sudo apt install -y csvkit
+fi
+
 
 # -----------------------------------------------------------------------------------------------------------
-# 3) Python3.10 Install: here we'll install Python3.10 - feel free to swap this for any version you'd like.
+# 2) Python3.10 Install: here we'll install Python3.10 - feel free to swap this for any version you'd like.
 # -----------------------------------------------------------------------------------------------------------
 
 # Check if software-properties-common is in the apt-cache
@@ -99,7 +159,7 @@ else
   sudo apt install -y software-properties-common
 fi
 
-# Add this apt repository for Python 3.9
+# Add this apt repository for Python 3.10
 if [ -n "$(ls /etc/apt/sources.list.d | grep deadsnakes)" ]
 then
   echo "ppa:deadsnakes/ppa apt repository present 🟢"
@@ -111,7 +171,7 @@ else
 fi
 
 # Now you can download Python3.10
-if [ -n "$(which python3.10)" ]
+if ( which python3.10 > /dev/null )
 then
   echo "Python3.10 already installed 🐍"
 else
@@ -120,9 +180,108 @@ else
 fi
 
 # Verify Python3.10 installation
-if [ -n "$(which python3.10)" ]
+if ( which python3.10 > /dev/null )
 then
   echo "$(python3.10 --version) 🐍 🚀 ✨"
 else
   echo "Python 3.10 was not installed successfully 🔴"
+fi
+
+
+# -----------------------------------------------------------------------------------------------------------
+# 3) Poetry Install: here we'll install and configure Poetry, as well as add Poetry to the PATH.
+# -----------------------------------------------------------------------------------------------------------
+
+# Install Poetry using the official installer
+if ( which poetry > /dev/null )
+then
+  echo "Poetry is already installed 🟢"
+else
+  echo "Installing Poetry 🧙‍♂️"
+  curl -sSL https://install.python-poetry.org | POETRY_VERSION=1.5.1 python3 -
+fi
+
+# Add Poetry to the path in the current user's .bashrc
+if ( poetry --version > /dev/null )
+then
+  echo "Poetry is already in PATH 🟢"
+else
+  echo -e "# Add Poetry (Python Package Manager) to PATH\nexport PATH="/home/$USER/.local/bin:$PATH"" >> ~/.bashrc
+  source ~/.bashrc
+fi
+
+# Configure Poetry to put build all virtual environments in the project's directory
+if [ "$(poetry config virtualenvs.in-project)" == "true" ]
+then
+  echo "Poetry already configured to create virtual envs within projects 🟢"
+else
+  echo "Configuring Poetry to create virtual envs in projects 🪐"
+  poetry config virtualenvs.in-project true
+fi
+
+
+# -----------------------------------------------------------------------------------------------------------
+# 4) Docker Install: here we'll install Docker
+# -----------------------------------------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------------------------------------
+# 4.1) Set up the repository: Before you install Docker Engine for the first time on a new host machine, 
+# you need to set up the Docker repository. Afterward, you can install and update Docker from the repository.
+# -----------------------------------------------------------------------------------------------------------
+
+# Pull the current machine's distro for GPG key targeting
+DISTRO=$(lsb_release -d | awk -F ' ' '{print tolower($2)}')
+
+# Add Docker’s official GPG key
+if [ -f /etc/apt/keyrings/docker.gpg ]
+then
+  echo 'Docker GPG Key already installed at /etc/apt/keyrings/docker.gpg 🟢'
+else
+  echo 'Installing Docker GPG Key at /etc/apt/keyrings/docker.gpg 🔧'
+  
+  # Create the /etc/apt/keyrings directory with appropriate permissions
+  sudo install -m 0755 -d /etc/apt/keyrings
+  
+  # Download the GPG key from Docker
+  curl -fsSL https://download.docker.com/linux/$DISTRO/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  sudo chmod a+r /etc/apt/keyrings/docker.gpg
+fi
+
+# Set up the repository
+if [ -f /etc/apt/sources.list.d/docker.list ] 
+then
+  echo 'docker.list repository already exists at /etc/apt/sources.list.d/docker.list 🟢'
+else
+  echo 'Installing docker.list repository at /etc/apt/sources.list.d/docker.list 🔧'
+  echo \
+    "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$DISTRO \
+    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+fi
+
+# -----------------------------------------------------------------------------------------------------------
+# 4.2) Install Docker Engine
+# -----------------------------------------------------------------------------------------------------------
+
+# Check if docker-ce is in the apt-cache
+if ( apt-cache show docker-ce > /dev/null )
+then
+  echo "docker-ce is already cached 🟢"
+else
+  sudo apt update
+fi
+
+# Install Docker Engine, containerd, and Docker Compose
+if ( docker --version > /dev/null )
+then
+  echo "Docker is already installed 🟢"
+  echo "Using $(docker --version)"
+else
+  echo "Installing Docker 🐳"
+
+  # Installs
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  
+  # Verify that the Docker Engine installation is successful by running the hello-world image
+  sudo docker run hello-world
 fi
