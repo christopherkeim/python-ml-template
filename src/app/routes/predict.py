b250dacf484cc3efe26c3cf9249e48f5c4b2e67e
Christopher Keim <122534665+christopherkeim@users.noreply.github.com>
2024-12-28 15:53:45 -0500

    API subrouters and `predict` router.

---
+++ src/app/routes/predict.py
@@ -0,0 +1,17 @@
+"""
+/predict
+"""
+
+from fastapi import APIRouter, Request
+
+from src.app.models import Input, Prediction, Predictor
+
+
+def router(predictor: Predictor) -> APIRouter:
+    router: APIRouter = APIRouter()
+
+    @router.get("", response_model=Prediction)
+    def get_prediction(x: float, y: float):
+        return predictor.predict(input=Input(x=x, y=y))
+
+    return router
